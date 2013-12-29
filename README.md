93f245ec7a855de6d5104d6923d70bb7e055e6bb
MarkNeves <mneves@gmail.com>
2013-12-29 16:37:06 -0600

    Added arrows

--- README.md
+++ README.md
@@ -2,9 +2,14 @@ coinbase-tracker
 ======
 A minimal console ticker for Coinbase exchange rates, written in Python.
 
+
+Screenshot
+------------
+![Screenshot](http://i.imgur.com/ooXJ5nD.png)
+
+
 Todo
 ------------
 1. Take [optional] input for set amount of Bitcoins (makes exchange rate vary)
 2. Improve UI
 3. Daemon mode
-
--- tracker.py
+++ tracker.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# [very simple] Coinbase Tracker v0.1
+# [very simple] Coinbase Tracker v0.2
 # by Mark Neves - mneves@gmail.com
 import json
 import requests
@@ -9,23 +9,33 @@ cb = "https://coinbase.com/api/v1/prices/"
 
 
 class loadStat(object):
+    last = {'buy': 0, 'sell': 0}
 
     def __init__(self, disp_type="sell"):
         self.disp_type = disp_type
 
     def val(self):
+        arrow = "  "
         r = json.loads(requests.get(cb + self.disp_type).text)
+        if(loadStat.last[self.disp_type] and loadStat.last[self.disp_type]
+                != r['subtotal']['amount']):
+            arrow = "\033[92m ▴\033[0m" if loadStat.last[self.disp_type] < r[
+                'subtotal']['amount'] else "\033[91m ▾\033[0m"
+        else:
+            loadStat.last[self.disp_type] = r['subtotal']['amount']
+
         return("\033[1m" + self.disp_type.title() + ": $"
-               + r['subtotal']['amount'] + "\033[0m ($" + r['total']['amount']
-               + " after fees)")
+               + r['subtotal']['amount'] + arrow +
+               "\033[0m ($" + r['total']['amount']
+               + " with fees)")
 
 
 def displayTicker():
-    threading.Timer(5.0, displayTicker).start()
+    threading.Timer(10.0, displayTicker).start()
     sys.stdout.write("\r" + loadStat('buy').val()
                      + "  ¦  " + loadStat('sell').val())
     sys.stdout.flush()
 
 print("\nCoinbase prices (per 1 Bitcoin):")
-print("-" * 72)
+print("-" * 74)
 displayTicker()
